38f7b1ccd26cdf42ae70435115ee547df0879830
NGPTechWorld <ngpsupp@gmail.com>
2023-12-25 09:43:36 +0300

    h

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,5 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Manar");
-        /*/Manar GG */
-        //fdfdfdl,dlc
-        //ffff
     }
 }
